d45d32eaaf1d71235abab26d783eb98baa73c03c
Nikolaus <github@koolych.ru>
2026-02-15 03:32:55 +1000

    Mutex implement

--- Wauncher/Program.cs
+++ Wauncher/Program.cs
@@ -1,16 +1,54 @@
 ﻿using Avalonia;
-using System;
+using Wauncher.Utils;
 
 namespace Wauncher
 {
     internal sealed class Program
     {
+        public static EventWaitHandle ProgramStarted;
+
         // Initialization code. Don't use any Avalonia, third-party APIs or any
         // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
         // yet and stuff might break.
         [STAThread]
-        public static void Main(string[] args) => BuildAvaloniaApp()
+        public static void Main(string[] args)
+        {
+            if (OnStartup(args) == false)
+            {
+                Environment.Exit(0);
+                return;
+            }
+
+            BuildAvaloniaApp()
             .StartWithClassicDesktopLifetime(args);
+        }
+
+        // Reference (COPYPASTA) 
+        // https://github.com/2dust/v2rayN/blob/d9843dc77502454b1ec48cec6244e115f1abd082/v2rayN/v2rayN.Desktop/Program.cs#L25-L52
+        private static bool OnStartup(string[]? Args)
+        {
+
+            if (Services.IsWindows())
+            {
+                var exePathKey = Services.GetMd5(Services.GetExePath());
+                var rebootas = (Args ?? []).Any(t => t == "rebootas");
+                ProgramStarted = new EventWaitHandle(false, EventResetMode.AutoReset, exePathKey, out var bCreatedNew);
+                if (!rebootas && !bCreatedNew)
+                {
+                    ProgramStarted.Set();
+                    return false;
+                }
+            }
+            else
+            {
+                _ = new Mutex(true, "Wauncher", out var bOnlyOneInstance);
+                if (!bOnlyOneInstance)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
 
         // Avalonia configuration, don't remove; also used by visual designer.
         public static AppBuilder BuildAvaloniaApp()

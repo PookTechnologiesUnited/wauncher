d34fe848cc7e729fdc529ba821c098e2c2075ab8
Nikolaus <github@koolych.ru>
2026-02-15 04:20:15 +1000

    Better publishing

--- publish.bat
+++ publish.bat
@@ -1,5 +1,15 @@
+@echo off
+for /f %%a in ('echo prompt $E^| cmd') do set "ESC=%%a"
+echo =============================
+echo %ESC%[42mBuilding...%ESC%[0m
 dotnet publish Launcher -c Release
 dotnet publish Wauncher -c Release
+echo =============================
+echo %ESC%[41mHashing...%ESC%[0m
 certutil -hashfile "Launcher\bin\Release\net8.0-windows7.0\win-x64\publish\launcher.exe" MD5
 certutil -hashfile "Wauncher\bin\Release\net8.0-windows7.0\win-x64\publish\wauncher.exe" MD5
-pause
+echo =============================
+echo %ESC%[1;43mCopying...%ESC%[0m
+set /p "destination=Copying destination (in quotations): "
+xcopy "Wauncher\bin\Release\net8.0-windows7.0\win-x64\publish\" %destination% /e /y
+timeout /t 5
